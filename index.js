1f1e2784c3cb729d165c895644e53a8fa1bf6436
Mia Adjei <adjei.m@gmail.com>
2021-06-21 17:30:42 -0400

    Small updates to starter code

--- index.js
+++ index.js
@@ -17,16 +17,16 @@ app.get('/token', (req, res) => {
   const identity = 'contributor'
 
   // Create a 'grant' identifying the Sync service instance for this app.
-  syncGrant = new SyncGrant({
-      serviceSid: process.env.TWILIO_SYNC_SERVICE_SID
+  const syncGrant = new SyncGrant({
+    serviceSid: process.env.TWILIO_SYNC_SERVICE_SID
   });
 
   // Create an access token which we will sign and return to the client,
   // containing the grant we just created and specifying their identity.
   const token = new AccessToken(
-      process.env.TWILIO_ACCOUNT_SID,
-      process.env.TWILIO_API_KEY_SID,
-      process.env.TWILIO_API_KEY_SECRET,
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_API_KEY_SID,
+    process.env.TWILIO_API_KEY_SECRET,
   );
 
   token.addGrant(syncGrant);
@@ -34,12 +34,12 @@ app.get('/token', (req, res) => {
 
   // Serialize the token to a JWT string and include it in a JSON response
   res.send({
-      identity: identity,
-      token: token.toJwt()
+    identity: identity,
+    token: token.toJwt()
   });
 });
 
 // Start the node.js server
 app.listen(port, () => {
-    console.log(`Twilio Sync Co-browsing App listening at http://localhost:${port}`);
+  console.log(`Twilio Sync Co-browsing App listening at http://localhost:${port}`);
 });
